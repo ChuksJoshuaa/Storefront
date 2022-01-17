368386fffb41bb8a1e25acae3822da8fc3854323
D-coder <chuksmbanasoj@gmail.com>
2022-01-17 03:57:08 +0100

    fix: Update views.py

--- playground/views.py
+++ playground/views.py
@@ -30,28 +30,3 @@ def say_yes(request):
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
-
-# celery commands
-# before that run the docker command to connect to redis
-# docker run -d -p 6379:6379 redis
-# after that python -m pip install redis
-# open another terminal to run the manage.py runserver
-# then run the below command in another terminal
-# celery -A <project> worker -l info -P gevent
-# open another terminal to run this command
-# celery -A <project> beat
-# python -m pip install flower
-# celery -A <project> flower
-# access the page
-
-# incase that one no work
-# pip install eventlet
-# celery - A <project> worker -l info -P eventlet
-
-# how to test specify the stuff to be tested
-# pytest store/tests/test_collections.py::TestCreateCollection
-# pytest -k anonymous
-
-# we use @pytest.mark.skip to skip some tests we dont want, and let it be at the top like a decorator.
-
-# Also we use ptw to run continous test
